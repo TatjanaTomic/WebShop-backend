63daf79f97c51a7100270a672407f097afb42eb3
TatjanaTomic <tatjanatomic997@yahoo.com>
2022-12-30 00:45:31 -0800

    First commit

---
+++ src/main/java/org/unibl/etf/ip/shop/WebShopBackendApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/unibl/etf/ip/shop/WebShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
